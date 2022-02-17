d1d1439073cb505bc62af99b9321c642f8efc4ab
Brandon Forsythe <bfforsythe@alaska.edu>
2022-02-16 21:17:50 -0900

    Finished A1, working on A2

--- HW02/HW02/HW02.cpp
+++ HW02/HW02/HW02.cpp
@@ -8,12 +8,13 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <iomanip>
 
 using namespace std;
 
 
 // InputNames creates a vector of length ten to be used later.
-
+/*
 void InputNames(vector<string>& names) {
 
 	for (int i = 0; i < 10; i++) {
@@ -70,7 +71,7 @@ int main() {
 	
 	return(0);
 
-}
+} */
 
 
 
@@ -95,3 +96,56 @@ for (int parse = 0; parse < vectorCopy.size(); parse++) {
 if (isNameHere == false) {
 	cout << "Your query does not exist";
 } */
+
+
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+//Brandon Forsythe
+// CS201 Redux HW02 A1
+// Money.cpp
+// 2/12/22
+
+/* void coinStar() {
+
+	int penny, nickel, quarter, dime, dollarcoin;
+
+	cout << "-----------------------------\n";
+	cout << "   Welcome to the Coinstar   \n";
+	cout << "-----------------------------\n";
+
+	cout << "\n\n Please Enter Your Change \n";
+
+	cout << "Pennies: ";
+	cin >> penny;
+
+	cout << "\n Nickels: ";
+	cin >> nickel;
+
+	cout << "\n Quarters: ";
+	cin >> quarter;
+
+	cout << "\n Dimes: ";
+	cin >> dime;
+
+	cout << "\n Dollar Coins: ";
+	cin >> dollarcoin;
+
+	cout << "You have: " << penny << " pennies, " << nickel << " nickels " << dime << " dimes, " << quarter << " quarters, and " << dollarcoin << " Dollar coins.\n";
+
+	float total = penny + (5 * nickel) + (25 * quarter) + (10 * dime) + (100 * dollarcoin);
+
+	cout << "Your total is: " << total << " cents. / $" << setprecision(5) <<  total / 100;
+}
+
+int main() {
+
+	coinStar();
+
+} */
+
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+// Brandon Forsythe
+// CS201 Redux HW02 A2
