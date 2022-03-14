8d9226fb3aed63205d29e48257425a5f3e49729f
Brandon Forsythe <bfforsythe@alaska.edu>
2022-03-14 13:05:47 -0800

    Began HW04

---
+++ HW04/HW04/HW04.cpp
@@ -0,0 +1,35 @@
+//Brandon Forsythe
+//CS201-Redux HW04
+//3/7/22
+
+
+
+
+#include "std_lib_facilites.h"
+
+bool ReadLine(string& str) {
+	getline(cin, str);
+
+	cout << str.empty();
+
+	return(0);
+}
+
+unsigned StringtoTokenWS(const string& input, vector<string>& tokens) {
+	
+	string str;
+	ReadLine(str);
+
+
+	std::istringstream(fullString);
+
+	return(0);
+}
+
+int main() {
+
+	string str;
+	vector<string> tokens;
+
+	StringtoTokenWS(str, tokens);
+}
---
+++ HW04/HW04/std_lib_facilites.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#ifndef std_lib_facilites.h
+
+#include<iostream>
+#include <iomanip>
+#include<sstream>
+#include<fstream>
+#include<string>
+#include <vector>
+#include <map>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <random>
+#include <sstream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+using std::getline;
+
+// keepWindowOpen (Keeps window open)
+inline void keepWindowOpen() {
+	string exit;
+	cin.clear();
+	cout << "Press Enter to exit the program" << endl;
+	getline(cin, exit);
+}
+
+//printString (Function to print a complete string with newline) (Testing purposes?)
+
+inline void printString(string debuggy) {
+	cout << debuggy << "\n";
+}
+
+
+//Nuclear option (I cannot come up with anything else I need for the moment. This blows up.
+
+inline void nuke() {
+	for (int heatDeath = 0; heatDeath <= 1000; heatDeath++) {
+		cout << "Goodbye!";
+	}
+}
+
+#endif
+
+
