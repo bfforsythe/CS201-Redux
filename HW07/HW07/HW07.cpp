67ebb82953570f6b51f755783a194b01408b2031
bfforsythe <bfforsythe@alaska.edu>
2022-04-13 15:58:49 -0800

    Finished HW07 (used code from final project last year)

--- HW07/HW07/HW07.cpp
+++ HW07/HW07/HW07.cpp
@@ -1,6 +1,7 @@
 #include "std_lib_facilites.h"
 #include "CImg.h"
-
+#include <fstream>
+#include <cstdio>
 // **************************************** HW07
 
 
@@ -9,29 +10,27 @@ using namespace cimg_library;
 
 const char *ASCII_List = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
 
-CImg<> rgb2gray(CImg<> color_img)
+CImg<> rgb2gray(CImg<> color)
 {
    
 
-    CImg<> gray_img(color_img.width(), color_img.height(), 1, 1, 0);
+    CImg<> gray_img(color.width(), color.height(), 1, 1, 0);
 
     
 
 
-    cimg_forXY(color_img, x, y) {
+    cimg_forXY(color, x, y) {
        
 
         // Triple Channel
 
-        int R = (int)color_img(x, y, 0, 0);
-        int G = (int)color_img(x, y, 0, 1);
-        int B = (int)color_img(x, y, 0, 2);
+        int R = (int)color(x, y, 0, 0);
+        int G = (int)color(x, y, 0, 1);
+        int B = (int)color(x, y, 0, 2);
 
         int gray_val = (int)(0.299 * R + 0.587 * G + 0.114 * B);
         gray_img(x, y, 0, 0) = gray_val;
     }
-
-    gray_img.normalize(0, 255);
     return gray_img;
 }
 
@@ -45,13 +44,36 @@ void print_gray2ascii(CImg<> gray_img, const char* file_name, int w, int h)
 
     cimg_forY(gray_img, y) {
         cimg_forX(gray_img, x) {
-            int val = gray_img(x, y, 0, 0) / sizeof(ASCII_LIST);
-            out << ASCII_LIST[val];
+            int val = gray_img(x, y, 0, 0) / sizeof(ASCII_List);
+            out << ASCII_List[val];
         }
         out << endl;
     }
+   
 
-    out.close();
+    
 }
 
+
+
+int main(void)
+{
+
+
+    // Init images
+    CImg<> img("parrot.bmp");
+
+    img = rgb2gray(img);
+
+    const char* file_name = "out.txt";
+    print_gray2ascii(img, file_name, 100, 40);
+
+    std::ifstream f("out.txt");
+
+    if (f.is_open())
+        std::cout << f.rdbuf();
+
+    return 0;
+
+}
 
