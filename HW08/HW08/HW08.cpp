72a2561b4a55c2a8588749cc575c1d258e0f5694
bfforsythe <bfforsythe@alaska.edu>
2022-04-18 15:47:06 -0800

    Finished HW08 A1

--- HW08/HW08/HW08.cpp
+++ HW08/HW08/HW08.cpp
@@ -7,7 +7,7 @@ using namespace cimg_library;
 
 
 
-
+/*
 class ppm {
 
 public:
@@ -37,7 +37,7 @@ public:
 
 
 
-int main(void)
+int main()
 {
 
     CImg<> img("parrot.bmp");
@@ -45,6 +45,51 @@ int main(void)
     rgb.ppm2ascii(img, "parrot.txt", 512, 512);
     
 
+} */
+
+//*********************************************************** HW02 A1
+
+
+class ppm {
+
+public:
+
+    const char* ASCII_List = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+
+    CImg<> rgb2gray(CImg<> color)
+    {
+
+
+        CImg<> gray_img(color.width(), color.height(), 1, 1, 0);
+
+
+
+
+        cimg_forXY(color, x, y) {
+
+
+            // Triple Channel
+
+            int R = (int)color(x, y, 0, 0);
+            int G = (int)color(x, y, 0, 1);
+            int B = (int)color(x, y, 0, 2);
+
+            int gray_val = (int)(0.2126 * R + 0.7152 * G + 0.0722 * B);
+            gray_img(x, y, 0, 0) = gray_val;
+        }
+        return gray_img;
+    }
+};
+
+
+
+int main()
+{
+
+    CImg<> img("parrot.bmp");
+    ppm rgb;
+  CImg<> grayImg = rgb.rgb2gray(img);
+    grayImg.save("parrot.pgm");
+
+
 }
-
-
